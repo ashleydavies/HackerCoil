4b77c8e8e6941d33eaddce3711dc6aee0b082275
Ashley Davies-Lyons <Ashley@daviesl.uk>
2015-11-20 01:49:47 +0000

    Now correctly loads map width, height and board data as a list of list of booleans

--- HackerCoil.hs
+++ HackerCoil.hs
@@ -2,6 +2,10 @@ import Network.HTTP
 import Data.List.Split
 import Data.List
 
+type GameBoardTile = Bool
+type GameBoard     = [[GameBoardTile]]
+type GameData      = (Int, Int, GameBoard)
+
 main :: IO ()
 main = do
   putStrLn "All data is transmitted over unencrypted HTTP. Please be aware of your network security!"
@@ -12,12 +16,28 @@ main = do
   let req = "http://www.hacker.org/coil/index.php?name="
           ++ name ++ "&password=" ++ pass
   response <- ((simpleHTTP (getRequest req)) >>= getResponseBody)
-  putStrLn $ getFlashVars response
+  putStrLn $ show $ parseFlashVars $ getFlashVars response
 
 getFlashVars :: String -> String
-getFlashVars = (flip (!!)) 1 .
-                  splitOn "\"" .
-                  head .
-                  filter (isPrefixOf "FlashVars") .
-                  map (dropWhile (\x -> x == ' ' || x == '\t')) .
-                  lines
+getFlashVars
+  = (flip (!!)) 1 . splitOn "\"" . head . filter (isPrefixOf "FlashVars") .
+        map (dropWhile (\x -> x == ' ' || x == '\t')) . lines
+
+parseFlashVars :: String -> GameData
+parseFlashVars vars
+  = (width, height, board)
+  where
+    varSplit = splitOneOf "&=" vars
+    width    = read $ varSplit !! 1
+    height   = read $ varSplit !! 3
+    board    = parseGameBoard (varSplit !! 5) width
+
+
+parseGameBoard :: String -> Int -> GameBoard
+parseGameBoard board width
+  = chunksOf width (map (\x -> x == 'X') board)
+
+{-
+splitEvery :: [a] -> Int -> [[a]]
+splitEvery [] n = []
+splitEvery li n = take n li ++ splitEvery (drop n li) n -}
